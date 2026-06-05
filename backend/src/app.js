a55e6290dd7d05785e0c0f2d5038294784b2a503
Ankit Nagar <iamankitnagar7@gmail.com>
2026-06-05 15:06:12 +0530

    fix cors and api url for production

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,8 +8,11 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true,
+  origin: [
+    "http://localhost:5173",
+    "https://ai-interview-prep-ebon.vercel.app"  // tera exact Vercel URL
+  ],
+  credentials: true
 }));
 
 //// require all routes here
